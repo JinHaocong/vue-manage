21539f7de03fbf6a75e85fb8d52377cf5850597a
JinHaocong <jinhaocong@outlook.com>
2022-08-16 10:31:32 +0800

    Vue 2 通用后台

--- src/views/user/UserPage.vue
+++ src/views/user/UserPage.vue
@@ -1,16 +1,125 @@
 <!--
  * @Author: Jin Haocong
  * @Date: 2022-08-15 10:22:26
- * @LastEditTime: 2022-08-15 10:23:46
+ * @LastEditTime: 2022-08-16 09:53:56
 -->
 <template>
-  <div>UserPage</div>
+  <div class="manage">
+    <el-dialog
+      :title="operateType === 'add' ? '新增用户' : '更新用户'"
+      :visible.sync="isShow"
+    >
+      <common-form
+        :formLabel="operateFormLabel"
+        :form="operateForm"
+        :inline="true"
+        ref="form"
+      >
+      </common-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="isShow = false">取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </div>
+    </el-dialog>
+    <div class="manage-header">
+      <el-button type="primary" @click="addUser">新增</el-button>
+      <common-form
+        :formLabel="formLabel"
+        :form="searchForm"
+        :inline="true"
+        ref="form"
+      >
+        <el-button type="primary" @click="getList">搜索</el-button>
+      </common-form>
+    </div>
+  </div>
 </template>
 <script>
+import CommonForm from "@/components/CommonForm.vue";
 export default {
+  components: { CommonForm },
   name: "UsePage",
   data() {
-    return {};
+    return {
+      operateType: "add",
+      isShow: false,
+      operateFormLabel: [
+        {
+          model: "name",
+          label: "姓名",
+          type: "input",
+        },
+        {
+          model: "age",
+          label: "年龄",
+          type: "input",
+        },
+        {
+          model: "sex",
+          label: "性别",
+          type: "select",
+          opts: [
+            {
+              label: "男",
+              value: 1,
+            },
+            {
+              label: "女",
+              value: 0,
+            },
+          ],
+        },
+        {
+          model: "birth",
+          label: "出生日期",
+          type: "date",
+        },
+        {
+          model: "addr",
+          label: "地址",
+          type: "input",
+        },
+      ],
+      operateForm: {
+        name: "",
+        addr: "",
+        age: "",
+        brith: "",
+        sex: "",
+      },
+      formLabel: [
+        {
+          model: "keyword",
+          label: "",
+          type: "input",
+        },
+      ],
+      searchForm: {
+        keyword: "",
+      },
+    };
+  },
+  methods: {
+    confirm() {},
+    addUser() {
+      this.isShow = true;
+      this.operateType = "add";
+      this.operateForm = {
+        name: "",
+        addr: "",
+        age: "",
+        brith: "",
+        sex: "",
+      };
+    },
+    getList() {},
   },
 };
 </script>
+<style lang="scss" scoped>
+.manage-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
